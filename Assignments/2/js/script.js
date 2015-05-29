bc4281fd6a0709c642d031be4828a25b1ac7c03a
Viacheslav Akhrem <akhrem.v@tut.by>
2015-05-29 15:13:49 +0300

    Problem 2: Currying (comments added)

--- Assignments/2/js/script.js
+++ Assignments/2/js/script.js
@@ -1,10 +1,12 @@
 (function() {
     'use strict';
 
+    // function returns string formed from passed arguments.
     function formString(x, y, z) {
         return x + ', ' + y + ', ' + z;
     }
 
+    // Currying function.
     function curry(fn) {
         var argumentsNumber = fn.length;
 
@@ -29,4 +31,7 @@
     var curriedFormString = curry(formString);
     console.log(curriedFormString('x')('y')('z'));
 
+// How is it differ from Partial Application?
+// Accepts arguments one per invocation.
+// Base function (formString() in current case) will not be invoked untill all arguments are supplied.
 })();
