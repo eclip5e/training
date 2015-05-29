c6ad5a4c0f45272204940bd3a833a024a911caaa
Viacheslav Akhrem <akhrem.v@tut.by>
2015-05-29 15:06:41 +0300

    Problem 1: Partial Application (comments and improvements)

--- Assignments/1/js/script.js
+++ Assignments/1/js/script.js
@@ -1,6 +1,7 @@
 (function() {
     'use strict';
 
+    // Function returns sum of all passed integers.
     function summarize() {
         var sum = 0;
         for (var i = 0; i < arguments.length; i++) {
@@ -9,19 +10,26 @@
         return sum;
     }
 
+    // Partial application function.
     function partialApplication(fn) {
+        // Referencing Array.prototype.slice() method.
+        var slice = Array.prototype.slice;
 
-        var argsArray = Array.prototype.slice.call(arguments, 1);
+        var argsArray = slice.call(arguments, 1);
 
         return function() {
-            return fn.apply(this, argsArray.concat(Array.prototype.slice.call(arguments, 0)));
+            return fn.apply(this, argsArray.concat(slice.call(arguments, 0)));
         };
     }
 
+    // Testing partial application function.
     var addHundred = partialApplication(summarize, 100);
     console.log("Passing multiple argumants to general (bound) function:", addHundred(1, 2, 3, 4, 5));
 
+    // Testing partial application function.
     var addFiveHundreds = partialApplication(summarize, 100, 200, 200);
     console.log("Passing multiple arguments to partial application function also:", addFiveHundreds(5, 10, 15));
 
+// Is there any JavaScript built-in alternative?
+// No.
 })();
